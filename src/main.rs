17b099ce498c9063712fde3de96e87b91b8a5888
Theo Fortune <theofortunefunction@gmail.com>
2024-12-05 12:40:45 +0100

    prim

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, 🦀 from CARGO!");
+    let x: i32 = -42;
+    let y: u64 = 100;
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
+    let e: i32 = 2147483647;
+    let i: i64 = 9223372036854775807;
+    println!("Maximum value of i32: {}", e);
+    println!("Maximum value of i64: {}", i);
+
+    let pi: f64 = 3.14;
+    println!("Value of pi: {}", pi);
 }
